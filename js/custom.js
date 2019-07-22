7ba1cf8acfaf754694e4fd70b9f2b18ad4c9247f
Priest <>
2019-07-22 06:20:41 +0300

    first commit

---
+++ js/custom.js
@@ -0,0 +1,216 @@
+(function($){
+	'use strict';
+
+/* --------------------------------------------------
+	Initialization
+-------------------------------------------------- */
+
+    // Initialize all functions when the document is ready.
+	$(document).ready(function(){
+
+		initNavbar();
+		initScroller();
+		initAnimation();
+
+	});
+
+	// Initialize functions after elements are loaded.
+	$(window).load(function() {
+
+		// Preloader
+		$('.preloader img').fadeOut(); // will first fade out the loading animation
+		$('.preloader').delay(350).fadeOut('slow', function() {
+
+		});
+
+	});
+
+
+/* --------------------------------------------------
+	Navigation | Navbar
+-------------------------------------------------- */
+	
+	function initNavbar(){
+
+		// Sticky Nav & Transparent Background
+		$(window).scroll(function(){
+			
+			if ($(window).scrollTop() > 20) {
+				$('nav').removeClass('navbar-trans', 300);
+				$('nav').removeClass('navbar-trans-dark');
+				$('nav').addClass('navbar-small', 300);
+			}
+			else {
+				$('nav:not(.mobile-nav)').addClass('navbar-trans', 300);
+				$('nav').removeClass('navbar-small', 300);
+
+				if ($('nav').hasClass('trans-helper')) {
+					$('nav:not(.mobile-nav)').addClass('navbar-trans-dark');
+				}
+			}
+
+			$('nav:not(.navbar-fixed-top)').removeClass('navbar-trans navbar-small navbar-trans-dark');
+
+		});
+
+
+		// Nav on mobile screens
+		$(window).resize(function() {
+	        if ($(window).width() <= 1259) {
+				$('nav').addClass('mobile-nav');		        
+		    } else {
+		    	$('nav').removeClass('mobile-nav');
+		    }
+
+    		if ($('nav').hasClass('mobile-nav')) {
+    			$('nav').removeClass('navbar-trans');
+    			$('nav').removeClass('navbar-trans-dark');
+    		} else {
+    			if ($(window).width() >= 1259 && $(window).top) {
+    				$('nav').addClass('navbar-trans');
+    			}
+    		}
+
+    		// Remove dropdown open on hover for small screens
+    		if ($('nav').hasClass('mobile-nav')) {
+
+    			$('.dropdown-toggle').on('mouseover', function(e){    
+    			        e.preventDefault();
+
+    			        $('.dropdown').removeClass('open');
+
+    			    e.stopPropagation();
+    			});
+    		}
+
+    		// Close mobile menu when clicked link
+    		// var isNotDropdown = $('nav:not(.mobile-nav)');
+
+    		if (!$('.nav a').hasClass('dropdown-toggle')) {
+
+    			$('.nav a').on('click', function(){ 
+			        if($('.navbar-toggle').css('display') !='none'){
+			            $(".navbar-toggle").trigger( "click" );
+			        }
+			    });
+
+    		}
+
+	    }).resize();
+
+		// Bugfix for iOS not scrolling on open menu
+	    $(".navbar-collapse").css({ maxHeight: $(window).height() - $(".navbar-header").height() + "px" });
+
+
+	} // initNavbar
+
+
+
+/* --------------------------------------------------
+	Scroll Nav
+-------------------------------------------------- */
+
+	function initScroller () {
+
+		$('#navbar').localScroll({
+			easing: 'easeInOutExpo'
+		});
+
+		$('#page-top').localScroll({
+			easing: 'easeInOutExpo'
+		});	
+	} // initScroller
+
+
+
+
+/* --------------------------------------------------
+	Parallax
+-------------------------------------------------- */
+
+	
+	function initParallax () {
+
+		var isSafari = /Safari/.test(navigator.userAgent) && /Apple Computer/.test(navigator.vendor);
+
+		if (!isSafari) {
+			$(".main-op").parallax("50%", 0.2);
+			$(".number-counters").parallax("50%", 0.2);
+			$(".cirlce-counters").parallax("50%", 0.3);
+			$(".client-list-parallax").parallax("50%", 0.4);
+			$(".ft-slider-parallax").parallax("50%", 0.4);
+			$(".testimonials-parallaxx").parallax("50%", 0.4);
+			$(".twitter-slider").parallax("50%", 0.4);
+			$(".login-2").parallax("50%", 0.2);
+		}		
+	}
+
+
+
+/* --------------------------------------------------
+	Animation
+-------------------------------------------------- */
+
+	function initAnimation () {
+		
+		new WOW().init();
+
+	}
+
+})(jQuery);
+
+
+
+/* --------------------------------------------------
+	Contact Form JS Validation & AJAX call 
+-------------------------------------------------- */
+$(function() {
+
+//	Regular Expressions
+var expEmail = /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[_a-z0-9-]+(\.[_a-z0-9-]+)*(\.[a-z]{2,4})$/;
+var	expLettersOnly = /^[A-Za-z ]+$/;
+
+//	Checks if a field has the correct length
+function validateLength ( fieldValue, minLength ) {
+	return ( $.trim( fieldValue ).length > minLength );
+}
+
+//	Validate form on typing
+$( '.form-ajax' ).on( 'keyup', 'input.validate-locally', function() {
+	validateField( $(this) );
+});
+
+//	AJAX call
+$( '.form-ajax' ).submit(function(e) {
+	e.preventDefault();
+	var $this = $( this ),
+			action = $this.attr( 'action' );
+
+	// The AJAX requrest
+	$.post(
+			action,
+			$this.serialize(),
+			function( data ) {
+				$( '.ajax-message' ).html( data );
+			}
+	);
+});
+
+  /* _____________________________________
+
+   Form Focus
+   _____________________________________ */
+
+  var formControl = $(".form-control");
+
+  formControl.on('focus', function () {
+    $(this).parent().addClass('focus')
+
+  });
+  formControl.on('blur', function () {
+    $(this).parent().removeClass('focus');
+  });
+
+});
+
+
